70b2c3525b5989edae7cc1a8755d77f743adcae9
Ana Luiza <analuizaramalho9@gmail.com>
2020-12-02 11:23:11 -0300

    Add validate fields login and register

--- DigitalHouseMarvelApp/app/src/main/java/com/example/digitalhousemarvelapp/login/view/LoginFragment.kt
+++ DigitalHouseMarvelApp/app/src/main/java/com/example/digitalhousemarvelapp/login/view/LoginFragment.kt
@@ -6,11 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.digitalhousemarvelapp.R
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginFragment : Fragment() {
     private lateinit var _view:View
+    private var _navController: NavController? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,17 +29,50 @@ class LoginFragment : Fragment() {
 
         _view=view
 
+        _navController=findNavController()
+
         val btnLogin = _view.findViewById<Button>(R.id.btnLogin_Login)
         btnLogin.setOnClickListener {
-            _view.findNavController().navigate(R.id.action_loginFragment_to_comicListFragment)
+            redirectHome()
         }
 
         val btnCreateAccount = _view.findViewById<Button>(R.id.btnCreateAccount_Login)
         btnCreateAccount.setOnClickListener {
-            _view.findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+            redirectRegister()
         }
 
 
     }
 
+
+    private fun validateFields(): Boolean {
+        val email = view?.findViewById<TextInputLayout>(R.id.textEmail_Login)
+        email?.error = null
+        val emailText = email?.editText?.text.toString()
+
+        val senha = view?.findViewById<TextInputLayout>(R.id.textPassword_Login)
+        senha?.error = null
+        val senhaText = senha?.editText?.text.toString()
+
+        if (emailText.trim() == "") {
+            email?.error = getString(R.string.erro_email)
+            return false
+        }
+
+        if (senhaText.trim() == "") {
+            senha?.error = getString(R.string.erro_senha)
+            return false
+        }
+
+        return true
+    }
+
+
+    private fun redirectHome() {
+       if(validateFields()) _navController!!.navigate(R.id.action_loginFragment_to_comicListFragment)
+    }
+
+    private fun redirectRegister() {
+        _navController!!.navigate(R.id.action_loginFragment_to_registerFragment)
+    }
 }
--- DigitalHouseMarvelApp/app/src/main/java/com/example/digitalhousemarvelapp/register/view/RegisterFragment.kt
+++ DigitalHouseMarvelApp/app/src/main/java/com/example/digitalhousemarvelapp/register/view/RegisterFragment.kt
@@ -6,11 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.digitalhousemarvelapp.R
+import com.google.android.material.textfield.TextInputLayout
 
 class RegisterFragment : Fragment() {
     private lateinit var _view:View
+    private var _navController: NavController? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,9 +29,62 @@ class RegisterFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         _view = view
 
+        _navController = Navigation.findNavController(view)
+
         val btnSave = _view.findViewById<Button>(R.id.btnSave_Register)
         btnSave.setOnClickListener {
-            _view.findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+           redirectLogin()
         }
+
+        setBackNavigation()
+    }
+
+
+
+    private fun setBackNavigation() {
+        _view.findViewById<ImageView>(R.id.imgBack_Register).setOnClickListener {
+            val navController = findNavController()
+            navController.navigateUp()
+        }
+    }
+
+    private fun redirectLogin() {
+        if(validateFieldsRegister())_navController!!.navigate(R.id.action_registerFragment_to_loginFragment)
+    }
+
+    private fun validateFieldsRegister(): Boolean {
+
+        val nome = _view?.findViewById<TextInputLayout>(R.id.textName_Register)
+        nome?.error = null
+        val nomeText = nome?.editText?.text.toString()
+
+        val email = _view?.findViewById<TextInputLayout>(R.id.textEmail_Register)
+        email?.error = null
+        val emailText = email?.editText?.text.toString()
+
+        val senha = _view?.findViewById<TextInputLayout>(R.id.textPassword_Register)
+        senha?.error = null
+        val senhaText = senha?.editText?.text.toString()
+
+        if (nomeText.trim() == "") {
+            nome?.error = getString(R.string.erro_nome)
+            return false
+        }
+
+        if (emailText.trim() == "") {
+            email?.error = getString(R.string.erro_email)
+            return false
+        }
+
+        if (senhaText.trim() == "") {
+            senha?.error = getString(R.string.erro_senha)
+            return false
+        }
+
+        if (senhaText.trim().length < 6) {
+            senha?.error = getString(R.string.minimo_senha)
+            return false
+        }
+        return true
     }
 }
